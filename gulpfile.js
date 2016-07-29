31b2b62e832ffc1ac8b400c4ea78fc469a4c7503
Aleksandr Bukhalo <aleksandr@bukhalo.com>
2016-07-29 16:33:58 +0300

    Minimal workflow for theme development

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+    gulp.src(['src/scss/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('application.css'))
+        .pipe(gulp.dest('stylesheets'))
+});
+
+gulp.task('scripts', function() {
+    gulp.src(['src/js/*.js'])
+        .pipe(concat('theme.js'))
+        .pipe(gulp.dest('javascripts'))
+});
+
+gulp.task('livereload', ['styles', 'scripts'], function() {
+
+    browserSync.init({
+        proxy: "http://127.0.0.1:8080/redmine/"
+    });
+
+    gulp.watch('src/scss/*.scss').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['livereload'], function() {
+    gulp.run('styles', 'scripts');
+});
